699be85a7160f8b7b13fa6c798fcb3b517e28c54
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2022-11-19 22:37:46 -0500

    0002 initial setup (#4)
    * Final stuff
    
    * first deploy
    
    * First Real Deploy
    
    * Test
    
    Co-authored-by: gallagherpat <106109156+gallagherpatrick@users.noreply.github.com>

---
+++ dist/index.06bc2ed7.js
@@ -0,0 +1,2 @@
+let e=!1;document.querySelector(".svg-icon").addEventListener("click",(function(){0==e?(document.querySelector("#nav").style="Display: Block;",e=!0):1==e&&(document.querySelector("#nav").style="Display: None;",e=!1)}));
+//# sourceMappingURL=index.06bc2ed7.js.map
